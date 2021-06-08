c58ad51f9773d4cbb94e81145e12c587c2bc2b42
Ang <ang@imjustgood.com>
2021-06-09 06:57:38 +0800

    Update config.py

--- Imgood/linepy/config.py
+++ Imgood/linepy/config.py
@@ -4,15 +4,16 @@ import re
 
 class Config(object):
     LINE_HOST_DOMAIN            = 'https://gd2.line.naver.jp'
-    LINE_OBS_DOMAIN               = 'https://obs-sg.line-apps.com'
-    LINE_TIMELINE_API              = 'https://gd2.line.naver.jp/mh/api'
-    LINE_TIMELINE_MH              = 'https://gd2.line.naver.jp/mh'
-    LINE_OBJECT_URL                = 'https://obs.line-scdn.net/'
-    LINE_LOGIN_QUERY_PATH  = '/api/v4p/rs'
-    LINE_AUTH_QUERY_PATH   = '/api/v4/TalkService.do'
+    LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
+    LINE_TIMELINE_API           = 'https://gd2.line.naver.jp/mh/api'
+    LINE_TIMELINE_MH            = 'https://gd2.line.naver.jp/mh'
+    LINE_API_PATH               =  "https://legy-jp.line.naver.jp/acct/lgn/sq/v1"
+    LINE_API_POLL               =  "https://legy-jp.line.naver.jp/acct/lp/lgn/sq/v1"
+    LINE_LOGIN_QUERY_PATH       = '/api/v4p/rs'
+    LINE_AUTH_QUERY_PATH        = '/api/v4/TalkService.do'
 
     LINE_API_QUERY_PATH_FIR     = '/S4'
-    LINE_POLL_QUERY_PATH_FIR = '/P4'
+    LINE_POLL_QUERY_PATH_FIR    = '/P4'
     LINE_CALL_QUERY_PATH        = '/V4'
     LINE_CERTIFICATE_PATH       = '/Q'
     LINE_CHAN_QUERY_PATH        = '/CH4'
@@ -21,7 +22,7 @@ class Config(object):
     LINE_LIFF_QUERY_PATH        = '/LIFF1'
 
     CHANNEL_ID = {
-        'LINE_TIMELINE': '1341209950',
+        'LINE_TIMELINE': '1341209850',
         'LINE_WEBTOON': '1401600689',
         'LINE_TODAY': '1518712866',
         'LINE_STORE': '1376922440',
@@ -29,8 +30,18 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96] # DESKTOPWIN
-    APP_VER     = '6.5.4'
+    HEADERS = {
+        "CHROMEOS": {
+             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50",
+             "x-line-application": "CHROMEOS\t2.4.5\tChrome OS\t1"
+        },
+        "DESKTOPWIN": {
+            "user-agent": "Line/7.0.0",
+            "x-line-application": "DESKTOPWIN\t7.0.0\tWindows\t10"
+        }        
+    }
+
+    APP_TYPE    = "DESKTOPWIN" #CHOOSE YOUR HEADERS
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'MR.ANG'
     SYSTEM_VER  = '10.0.0'
@@ -39,5 +50,5 @@ class Config(object):
 
     def __init__(self):
         #sniff chrome headers and use those instead, because these will get you messagebanned
-        self.USER_AGENT = 'Line/%s' % self.APP_VER
-        self.APP_NAME = '%s\t%s\t%s\t%s' % (self.APP_TYPE, self.APP_VER, self.SYSTEM_NAME, self.SYSTEM_VER)
+        self.USER_AGENT = self.HEADERS[self.APP_TYPE]["user-agent"]
+        self.APP_NAME = self.HEADERS[self.APP_TYPE]["x-line-application"]
